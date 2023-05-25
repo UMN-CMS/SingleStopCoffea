1f625a47345c01f2358d3443ddbef9f0c30a8fbd
kapsiak <kapsi002@umn.edu>
2023-05-25 08:17:02 -0500

    Nicer cuts

--- plotter.py
+++ plotter.py
@@ -9,9 +9,10 @@ import multiprocessing
 from functools import partial
 from rich.progress import track
 import concurrent.futures
-from typing import List, Iterable, Callable
+from typing import List, Iterable, Callable, Union, Dict
 from dataclasses import dataclass
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import itertools as it
 
 
 font_dirs = ["./fonts"]
@@ -22,7 +23,7 @@ for font in font_files:
 plt.style.use("style.mplstyle")
 
 
-def getHistograms(path):
+def getHistograms(path: Union[Path, str]) -> Dict[str, hist.Hist]:
     with open(path, "rb") as f:
         r = pickle.load(f)
     return r
@@ -31,7 +32,7 @@ def getHistograms(path):
 all_hists = getHistograms("output.pkl")
 
 
-def addPrelim(ax):
+def addPrelim(ax: mpl.axis.Axis) -> mpl.axis.Axis:
     ax.text(
         0.02,
         0.98,
@@ -42,12 +43,11 @@ def addPrelim(ax):
         verticalalignment="top",
         transform=ax.transAxes,
     )
+    return ax
 
 
-def countHistAxes(hist):
+def countHistAxes(hist: hist.Hist) -> int:
     return len(hist.axes)
-
-
 
 
 @dataclass
@@ -55,24 +55,43 @@ class SampleHistogram:
     name: str
     hist: hist.Hist
 
-def draw1DHistogram(ax, vals, edges, label=None, orientation="vertical", **kwargs):
+
+def draw1DHistogram(
+    ax: mpl.axis.Axis,
+    vals: np.ndarray,
+    edges: np.ndarray,
+    label: str = None,
+    orientation: str = "vertical",
+    **kwargs
+) -> mpl.axis.Axis:
     widths = np.diff(edges)
-    ret = ax.hist(edges[:-1], bins=edges, weights=vals, label=label, orientation=orientation)
+    ret = ax.hist(
+        edges[:-1], bins=edges, weights=vals, label=label, orientation=orientation
+    )
     return ret
 
-def drawScatter(ax, vals, edges, yerr=None, label=None, **kwargs):
-    locs =  edges[:-1] + np.diff(edges) / 2
+
+def drawScatter(
+    ax: mpl.axis.Axis,
+    vals: np.ndarray,
+    edges: np.ndarray,
+    yerr: np.ndarray = None,
+    label: str = None,
+    **kwargs
+) -> mpl.axis.Axis:
+    locs = edges[:-1] + np.diff(edges) / 2
     ret = ax.errorbar(locs, vals, yerr=yerr, label=label, fmt="o")
     return ret
 
+
 def make1DPlot(
-    ax,
+    ax: mpl.axis.Axis,
     hists: Iterable[SampleHistogram],
     draw_function: Callable,
-    stack=True,
-    sort=True,
-    errors=False,
-):
+    stack: bool = True,
+    sort: bool = True,
+    errors: bool = False,
+) -> mpl.axis.Axis:
     hists = list(hists)
     hists = list(sorted(hists, key=lambda x: x.hist.sum().value) if sort else hists)
     bottom = None
@@ -81,29 +100,28 @@ def make1DPlot(
         x = sample_hist.hist.to_numpy()
         vals, edges = sample_hist.hist.to_numpy()
         variance = sample_hist.hist.view().variance if errors else None
-        draw_function(ax, vals, edges, label=sample_hist.name, bottom=bottom, yerr=variance)
+        draw_function(
+            ax, vals, edges, label=sample_hist.name, bottom=bottom, yerr=variance
+        )
         if stack:
             bottom = bottom + vals if bottom is not None else vals
     return ax
 
-def make2DHistogram(ax, hists):
-    pass
 
-
-def makeStackPlot(ax, histogram, signal=None, dset_axis=0):
+def makeStackPlot(
+    ax: mpl.axis.Axis, histogram: hist.Hist, signal: Iterable[str] = None
+) -> mpl.axis.Axis:
     signal = [] if signal is None else signal
     samples = list(s for s in histogram.axes[0] if s not in signal)
     samples = ["TT2018"]
     ax = make1DPlot(
-        ax,
-        [SampleHistogram(s, histogram[s, ...]) for s in samples],
-        draw1DHistogram
+        ax, [SampleHistogram(s, histogram[s, ...]) for s in samples], draw1DHistogram
     )
     ax = make1DPlot(
         ax,
-        [SampleHistogram(s,histogram[s, ...]) for s in signal],
+        [SampleHistogram(s, histogram[s, ...]) for s in signal],
         drawScatter,
-        errors=True
+        errors=True,
     )
     ax.set_yscale("log")
     xax = next(x for x in histogram.axes if x.name != "dataset")
@@ -115,18 +133,17 @@ def makeStackPlot(ax, histogram, signal=None, dset_axis=0):
     return ax
 
 
-
-
 signals = [
-                "signal_2000_600_Skim",
-                "signal_1500_400_Skim",
-                "signal_2000_1900_Skim",
-                "signal_2000_400_Skim",
-                "signal_2000_900_Skim",
-                "signal_1500_600_Skim",
-            ]
+    "signal_2000_600_Skim",
+    "signal_1500_400_Skim",
+    "signal_2000_1900_Skim",
+    "signal_2000_400_Skim",
+    "signal_2000_900_Skim",
+    "signal_1500_600_Skim",
+]
 
-def makefig(o, vals):
+
+def makefig(o: Union[Path, str], vals) -> None:
     n, h = vals
     if countHistAxes(h) == 2:
         f, ax = plt.subplots()
@@ -141,44 +158,76 @@ def makefig(o, vals):
         addPrelim(ax)
         f.savefig(o / n)
 
-def make2DHist(ax, histogram):
-    vals, e1,e2= histogram.to_numpy()
+
+def make2DHist(ax: mpl.axis.Axis, histogram: hist.Hist) -> mpl.axis.Axis:
+    vals, e1, e2 = histogram.to_numpy()
     ex = (e1[1:] + e1[:-1]) / 2
     ey = (e2[1:] + e2[:-1]) / 2
-    vx,vy = np.meshgrid(ex,ey)
+    vx, vy = np.meshgrid(ex, ey)
     x = vx.ravel()
     y = vy.ravel()
     w = vals.T.ravel()
-    ax.hist2d(x,y, bins=np.array([e1,e2]), weights=w)
+    ax.hist2d(x, y, bins=np.array([e1, e2]), weights=w)
     ax.set_xlabel(histogram.axes[0].label)
     ax.set_ylabel(histogram.axes[1].label)
     return ax
 
 
-
-def makefig2D(o, h):
-    print(h)
-    f, ax = plt.subplots()
+def make2DProjection(
+    ax: mpl.axis.Axis,
+    h: hist.Hist,
+    vlines: Iterable[int] = None,
+    hlines: Iterable[int] = None,
+) -> mpl.axis.Axis:
+    vlines = list(vlines) if vlines is not None else []
+    hlines = list(hlines) if hlines is not None else []
     ax = make2DHist(ax, h)
     divider = make_axes_locatable(ax)
-    ax_histx = divider.append_axes("top", 1.2, pad=0.1, sharex=ax)
-    ax_histy = divider.append_axes("right", 1.2, pad=0.1, sharey=ax)
+    ax_histx = divider.append_axes("top", 2, pad=0.4, sharex=ax)
+    ax_histy = divider.append_axes("right", 2, pad=0.4, sharey=ax)
     ax_histx.xaxis.set_tick_params(labelbottom=False)
     ax_histy.yaxis.set_tick_params(labelleft=False)
     binwidth = 0.25
+    x, y = h[:, sum].to_numpy()
+    draw1DHistogram(ax_histx, x, y)
+    x, y = h[sum, :].to_numpy()
+    draw1DHistogram(ax_histy, x, y, orientation="horizontal")
+    for a, l in it.product([ax, ax_histx], vlines):
+        a.axvline(x=l)
+    for a, l in it.product([ax, ax_histy], hlines):
+        a.axhline(y=l)
+    return ax, divider
 
-    draw1DHistogram(ax_histx, *h[:,sum].to_numpy())
-    draw1DHistogram(ax_histy, *h[sum,:].to_numpy(), orientation="horizontal") 
-    f.tight_layout()
-    f.savefig(o / "test.pdf")
 
+def make2DSlicedProjection(
+    ax: mpl.axis.Axis,
+    h: hist.Hist,
+    h_cut: hist.Hist,
+    vlines: Iterable[int] = None,
+    hlines: Iterable[int] = None,
+) -> mpl.axis.Axis:
+    ax, div = make2DProjection(ax,h,vlines,hlines)
+    ax2 = div.append_axes("right", 2, pad=0.4, sharey=ax)
+    x, y = h_cut.to_numpy()
+    draw1DHistogram(ax2, x, y, orientation="horizontal")
+    ax2.yaxis.set_tick_params(labelleft=False)
+    return ax
 
 
 if __name__ == "__main__":
     outdir = Path("plots")
     outdir.mkdir(parents=True, exist_ok=True)
     # makefig(outdir, ("m04_m", all_hists["m04_m"]))
-    makefig2D(outdir, all_hists["m14_vs_m04"]["QCD2018", ...])
+    fig, ax = plt.subplots(1, 1, figsize=(12, 10))
+    #ax = make2DProjection(ax, all_hists["m14_vs_m04"]["QCD2018", ...], [1000])
+    h = all_hists["m14_vs_m04"]["signal_2000_900_Skim",...]
+    h = all_hists["m14_vs_m04"]["QCD2018",...]
+    cutlower = 700
+    cutupper = 1200
+    h2 = h[hist.loc(cutlower):hist.loc(cutupper):sum, :] 
+    print(h2.__repr__())
+    ax = make2DSlicedProjection(ax, h, h2, [cutlower,cutupper])
+    fig.savefig(outdir / "test.pdf")
     # with concurrent.futures.ProcessPoolExecutor() as p:
     #    futs = p.map(partial(makefig, outdir), list(all_hists.items())[0:2])
     #    for f in track(futs):
