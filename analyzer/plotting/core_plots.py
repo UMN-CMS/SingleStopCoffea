fefebc6449adfb953f422eccd3ddc1486e6b2b38
Charlie Kapsiak <kapsi002@umn.edu>
2023-07-26 07:34:10 -0500

    Starting to move plotting stuff

---
+++ analyzer/plotting/core_plots.py
@@ -0,0 +1,103 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.font_manager as font_manager
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import numpy as np
+import importlib.resources as imp_res
+from . import static
+from dataclasses import dataclass
+from functools import partial, wraps
+import itertools as it
+import hist
+
+
+
+
+def loadStyles():
+    style = imp_res.files(static) / "style.mplstyle" 
+
+    font_dirs = [imp_res.files(static) / "fonts" ]
+    font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
+    for font in font_files:
+        font_manager.fontManager.addfont(font)
+    plt.style.use(style)
+
+def addPrelim(ax: mpl.axis.Axis) -> mpl.axis.Axis:
+    ax.text(
+        0.02,
+        0.98,
+        "CMS Preliminary",
+        style="italic",
+        fontweight="bold",
+        horizontalalignment="left",
+        verticalalignment="top",
+        transform=ax.transAxes,
+    )
+    return ax
+
+def histRank(hist):
+    return len(hist.axes)
+
+
+@dataclass
+class PlotObject:
+    name: str
+    hist: hist.Hist
+
+def autoSplit(func):
+    @wraps(func)
+    def inner(ax, hist, cat_axis=None, **kwargs):
+        if cat_axis is None:
+            if isinstance(hist, PlotObject):
+                return func(ax,hist,**kwargs)
+            else:
+                return func(ax,PlotObject(hist.name, hist),**kwargs)
+        else:
+            to_plot = [PlotObject(n,hist[{cat_axis:n}]) for n in hist.axes[cat_axis] ]
+            to_plot = reversed(sorted(to_plot, key=lambda x: x.hist.sum().value))
+            for p in to_plot:
+                ax = func(ax,p,**kwargs)
+            return ax
+    return inner
+
+def magicPlot(func):
+    @wraps(func)
+    def inner(first, *args, **kwargs):
+        if isinstance(first, plt.Axes):
+            return func(first,*args,**kwargs)
+        else:
+            fig, ax = plt.subplots()
+            return fig, func(ax, first, *args, **kwargs)
+    return inner
+
+
+@magicPlot
+@autoSplit
+def drawAsScatter(ax, p, yerr=True):
+    hist = p.hist
+    x = hist.axes.centers[0]
+    y = hist.values()
+    var = np.sqrt(hist.variances())
+    if yerr:
+        ax.errorbar(x, y, yerr=var, label=p.name, fmt="+")
+    else:
+        ax.scatter(x, y, label=p.name, marker="+")
+    return ax
+
+
+
+@magicPlot
+@autoSplit
+def drawAs1DHist(ax, plot_object):
+    h = plot_object.hist
+    a = h.axes[0]
+    edges = a.edges
+    widths = a.widths
+    vals= h.values()
+    yerr= np.sqrt(h.variances())
+    ret = ax.bar(edges[:-1], vals, width=widths,yerr=yerr, label=plot_object.name, align="edge")
+    # print(h)
+    # if yerr is not None:
+    #    centers = (edges[:-1] + edges[1:])/2
+    #    ret = ax.errorbar(centers, vals, yerr=yerr, color=h.color,  fmt="none")
+    return ax
