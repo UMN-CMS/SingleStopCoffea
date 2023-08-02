e987fc2a6072ebb330e79aabd0a2ae2c214188a3
Shardul Rao <shardulr2931@gmail.com>
2023-08-02 11:44:19 -0500

    Separate categories for num b and dRbb12

--- analyzer/modules/categories.py
+++ analyzer/modules/categories.py
@@ -14,28 +14,28 @@ def njetCategory(events,data):
     a = hist.axis.IntCategory([4, 5, 6], name="number_jets", label="NJets")
     return (a, ak.num(events.good_jets, axis=1))
 
-@analyzerModule("jetpT_category", ModuleType.Categories)
+@analyzerModule("jetpT_300", ModuleType.Categories)
 def jetpTCategory(events, data):
 	good_jets = events.good_jets
 	filled_jets = ak.pad_none(good_jets, 4, axis=1)
-	a = hist.axis.IntCategory([0, 1], name = 'jetpT_300', label = 'jetpT300')
+	a = hist.axis.IntCategory([0, 1], name = 'jetpT300', label = 'jetpT300')
 	return (a, ak.fill_none(filled_jets[:, 0].pt > 300, False))
 
-@analyzerModule("nJetRequirement_category", ModuleType.Categories)
+@analyzerModule("nJets_456", ModuleType.Categories)
 def nJetReqCategory(events, data):
 	good_jets = events.good_jets
-	a = hist.axis.IntCategory([0, 1], name = 'nJetRequirement', label = 'nJets')
+	a = hist.axis.IntCategory([0, 1], name = 'nJets456', label = 'nJets')
 	return (a, (ak.num(good_jets) >= 4) & (ak.num(good_jets) <= 6))
 
 
-@analyzerModule("lepton_category", ModuleType.Categories)
+@analyzerModule("noLeptons", ModuleType.Categories)
 def leptonCategory(events, data):
 	good_muons = events.good_muons
 	good_electrons = events.good_electrons
-	a = hist.axis.IntCategory([0, 1], name = 'is_lepton', label = 'isLepton')
+	a = hist.axis.IntCategory([0, 1], name = 'isLepton', label = 'isLepton')
 	return (a, (ak.num(good_electrons) == 0) & (ak.num(good_muons) == 0))
 
-@analyzerModule("dRJets_category", ModuleType.Categories)
+@analyzerModule("dRJets_24", ModuleType.Categories)
 def dRJetsCategory(events, data):
 	good_jets = events.good_jets
 	filled_jets = ak.pad_none(good_jets, 4, axis=1)
@@ -43,16 +43,36 @@ def dRJetsCategory(events, data):
 	a = hist.axis.IntCategory([0, 1], name = 'dRJets', label = 'dRJets')
 	return (a, (top_two_dr < 4) & (top_two_dr > 2))
 
-@analyzerModule("bSelection", ModuleType.Selection)
-def bSelection(events, selection):
+@analyzerModule("bTag_312", ModuleType.Categories)
+def bTag312(events, data):
 	mediumBs = events.med_bs
 	tightBs = events.tight_bs
-	selection.add("3bjet", ((ak.num(tightBs) >= 3).to_numpy()))
-	print("tight bs")
+	a = hist.axis.IntCategory([0, 1], name = '312Bs', label = '312Bs')
+	return (a, (ak.num(mediumBs) >= 2) & (ak.num(tightBs) >= 1))
+
+@analyzerModule("bTag_313", ModuleType.Categories)
+def bTag313(events, data):
+	tightBs = events.tight_bs
+	a = hist.axis.IntCategory([0, 1], name = '313Bs', label = '313Bs')
+	return (a, (ak.num(tightBs) >= 3))
+
+@analyzerModule("dRbb12_312", ModuleType.Categories)
+def dRbb312(events, data):
+	mediumBs = events.med_bs
+	mediumBs = ak.pad_none(mediumBs, 2, axis = 1)
+	dRbb12 = ak.fill_none(mediumBs[:, 0].delta_r(mediumBs[:, 1]), 0)
+	a = hist.axis.IntCategory([0, 1], name = 'dRbb_312', label = 'dRbb_312')
+	return (a, (dRbb12 < 1))
+
+@analyzerModule("dRbb12_313", ModuleType.Categories)
+def dRbb313(events, data):
+	tightBs = events.tight_bs
 	tightBs = ak.pad_none(tightBs, 2, axis = 1)
-	dRbb12 = ak.fill_none(tightBs[:, 0].delta_r(tightBs[:, 1]), False)
-	selection.add('dRbb12', (dRbb12 < 1).to_numpy())
-	print("dRbb12")
+	dRbb12 = ak.fill_none(tightBs[:, 0].delta_r(tightBs[:, 1]), 0)
+	a = hist.axis.IntCategory([0, 1], name = 'dRbb_313', label = 'dRbb_313')
+	return (a, (dRbb12 < 1))
+
+@analyzerModule("triggers", ModuleType.Selection)
+def triggers(events, selection):
 	selection.add("hlt", (events.HLT.PFHT1050 | events.HLT.AK8PFJet400_TrimMass30).to_numpy())
-	print("HT trigger")
 	return selection
