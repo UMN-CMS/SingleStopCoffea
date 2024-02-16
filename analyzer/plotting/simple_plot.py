062fb3da51682be4f913cb18d0554223a4646c57
Charlie Kapsiak <kapsi002@umn.edu>
2024-02-16 08:43:06 -0600

    Reworking simpleplot

--- analyzer/plotting/simple_plot.py
+++ analyzer/plotting/simple_plot.py
@@ -5,12 +5,46 @@ import hist
 import matplotlib.pyplot as plt
 from analyzer.plotting.styles import *
 from analyzer.plotting.core_plots import *
+import analyzer.core as ac
 from analyzer.datasets import SampleManager
 from pathlib import Path
 import logging
 import logging.handlers
 from enum import Enum, auto
 import atexit
+
+
+class _Split(object):
+    def __new__(cls):
+        return NoParam
+
+    def __reduce__(self):
+        return (_NoParamType, ())
+
+
+Split = object.__new__(_Split)
+
+
+def reformatHist(
+    hist,
+    axis_opts,
+):
+    h = hist
+    axes_names = {x.name for x in h.axes}
+    for n in all_axis_opts.keys():
+        if n not in axes_names:
+            raise KeyError(f"Name {n} is not an axis in {h}")
+    to_split = [x for x, y in all_axis_opts.items() if y is Split]
+    all_axis_opts = {x: y for x, y in all_axis_opts.items() if y is not Split}
+    h = h[all_axis_opts]
+    if to_split:
+        split_axes = [list(x) for x in h.axes if x.name in to_split]
+        split_names = [x.name for x in h.axes if x.name in to_split]
+        for combo in it.product(*split_axes):
+            f = dict(zip(split_names, (hist.loc(x) for x in combo)))
+            return h[f]
+    else:
+        return h
 
 
 def plotPulls(plotobj_pred, plotobj_obs, coupling, lumi):
@@ -60,7 +94,7 @@ def plot1D(
     for o in background_plobjs:
         drawAs1DHist(ax, o, yerr=False)
     for o in signal_plobjs:
-        #drawAs1DHist(ax, o, yerr=False)
+        # drawAs1DHist(ax, o, yerr=False)
         if sig_style == "scatter":
             drawAsScatter(ax, o, yerr=True)
         elif sig_style == "hist":
@@ -111,17 +145,7 @@ def plot2D(
     return fig
 
 
-class _Split(object):
-    def __new__(cls):
-        return NoParam
-
-    def __reduce__(self):
-        return (_NoParamType, ())
-
-
 class Plotter:
-    Split = object.__new__(_Split)
-
     def _createLogger(self):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
@@ -133,7 +157,7 @@ class Plotter:
 
     def __init__(
         self,
-        filenames,
+        input_data,
         outdir,
         target_lumi=None,
         default_backgrounds=None,
@@ -150,15 +174,18 @@ class Plotter:
         self.sample_manager = SampleManager()
         self.sample_manager.loadSamplesFromDirectory(dataset_dir)
 
-        filenames = [filenames] if isinstance(filenames, str) else list(filesnames)
-        results = [pkl.load(open(f, "rb")) for f in filenames]
+        if isinstance(input_data, ac.AnalysisResult):
+            results = [input_data]
+        else:
+            filenames = (
+                [input_data] if isinstance(input_data, str) else list(input_data)
+            )
+            results = [pkl.load(open(f, "rb")) for f in filenames]
 
         self.target_lumi = (
             target_lumi
             or self.sample_manager.getSet(list(results[0].results.keys())[0]).getLumi()
         )
-
-        self.coupling = coupling
 
         self.histos = accumulate(
             [
@@ -166,7 +193,9 @@ class Plotter:
                 for f in results
             ]
         )
-        print(self.histos)
+
+        self.coupling = coupling
+
         used_samples = set(it.chain.from_iterable(x.results.keys() for x in results))
         lumis = [
             round(self.sample_manager.getSet(x).getLumi(), 4) for x in used_samples
@@ -185,8 +214,9 @@ class Plotter:
                 "The underlying sampels have different luminosities, and you are not performing scaling"
             )
 
-        self.outdir = Path(outdir)
-        self.outdir.mkdir(exist_ok=True, parents=True)
+        if outdir:
+            self.outdir = Path(outdir)
+            self.outdir.mkdir(exist_ok=True, parents=True)
 
     def __call__(self, *args, **kwargs):
         self.doPlot(*args, **kwargs)
@@ -203,8 +233,12 @@ class Plotter:
             self.sample_manager[hist_pred],
         )
         fig = plotPulls(hppo, hopo, self.coupling, self.target_lumi)
-        fig.savefig(self.outdir / f"pull_{hist_obs}_{hist_pred}.pdf")
-        plt.close(fig)
+        if self.outdir:
+            fig.savefig(self.outdir / f"pull_{hist_obs}_{hist_pred}.pdf")
+            plt.close(fig)
+            return None
+        else:
+            return fig
 
     def plotRatio(self, target, hist_obs, hist_pred):
         ho = self.histos[target][hist_obs, ...]
@@ -218,8 +252,11 @@ class Plotter:
             self.sample_manager[hist_pred],
         )
         fig = plotPulls(hppo, hopo, self.coupling, self.target_lumi)
-        fig.savefig(self.outdir / f"pull_{hist_obs}_{hist_pred}.pdf")
-        plt.close(fig)
+        if self.outdir:
+            fig.savefig(self.outdir / f"pull_{hist_obs}_{hist_pred}.pdf")
+            plt.close(fig)
+        else:
+            return fig
 
     def doPlot(
         self,
@@ -252,9 +289,11 @@ class Plotter:
                 new_name = add_name + "cuts__" + "_".join(str(x) for x in combo)
                 f = dict(zip(split_names, (hist.loc(x) for x in combo)))
                 to_pass = h[f]
-                self.__doPlot(hist_name, to_pass, *args, **kwargs, add_name=new_name)
+                return self.__doPlot(
+                    hist_name, to_pass, *args, **kwargs, add_name=new_name
+                )
         else:
-            self.__doPlot(hist_name, h, *args, **kwargs, add_name=add_name)
+            return self.__doPlot(hist_name, h, *args, **kwargs, add_name=add_name)
 
     def __doPlot(
         self,
@@ -262,7 +301,7 @@ class Plotter:
         hist,
         sig_set,
         bkg_set=None,
-        scale="log",
+        scale=None,
         title="",
         add_name="",
         normalize=False,
@@ -272,6 +311,9 @@ class Plotter:
         xlabel_override=None,
     ):
         bkg_set = bkg_set if bkg_set is not None else self.default_backgrounds
+        if not scale:
+            scale = "linear"
+
         if add_label is None and add_name:
             add_label = add_name.title()
         self.logger.info(f"Now plotting {hist_name}")
@@ -285,8 +327,6 @@ class Plotter:
         signal_plobjs = createPlotObjects(
             hc, "dataset", self.sample_manager, cat_filter="signal"
         )
-        print(signal_plobjs, background_plobjs)
-
         if len(hist.axes) == 2:
             fig = plot1D(
                 signal_plobjs,
@@ -297,12 +337,18 @@ class Plotter:
                 xlabel_override=xlabel_override,
                 add_label=add_label,
                 top_pad=top_pad,
+                scale=scale,
             )
             fig.tight_layout()
-            fig.savefig(self.outdir / f"{add_name}{hist_name}.pdf")
-            plt.close(fig)
+            if self.outdir:
+                fig.savefig(self.outdir / f"{add_name}{hist_name}.pdf")
+                plt.close(fig)
+                return None
+            else:
+                return fig
 
         elif len(hist.axes) == 3:
+            ret = []
             for x in hc.axes[0]:
                 realh = hc[{"dataset": x}]
                 po = PlotObject(realh, x, self.sample_manager[x].style)
@@ -312,8 +358,12 @@ class Plotter:
                     self.target_lumi,
                     sig_style=sig_style,
                     add_label=add_label,
+                    scale=scale,
                 )
-                name = hist.name
                 fig.tight_layout()
-                fig.savefig(self.outdir / f"{add_name}{hist_name}_{x}.pdf")
-                plt.close(fig)
+                if self.outdir:
+                    fig.savefig(self.outdir / f"{add_name}{hist_name}_{x}.pdf")
+                    plt.close(fig)
+                else:
+                    ret.append(fig)
+            return ret
