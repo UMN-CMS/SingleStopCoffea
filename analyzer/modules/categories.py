a1afcc210e806b6212b50f246a08b60d54831bfd
Shardul Rao <shardulr2931@gmail.com>
2024-03-28 21:36:44 -0500

    Revert "Changes to categories.py modules"
    This reverts commit 512b0d586f79cb462b3d7071e5c96b7c95f733ae.
    
    Going back to earlier version in order to run data single muon stuff

--- analyzer/modules/categories.py
+++ analyzer/modules/categories.py
@@ -7,104 +7,89 @@ from analyzer.modules.axes import *
 
 @analyzerModule("dataset_category", categories="category", depends_on=["objects"])
 def datasetCategory(events, analyzer):
-  analyzer.histogram_builder.addCategory(dataset_axis, analyzer.setname)
-  return events, analyzer
+    analyzer.histogram_builder.addCategory(dataset_axis, analyzer.setname)
+    return events, analyzer
 
 
-njets_axis = hist.axis.IntCategory([4, 5, 6], name="number_jets", label="NJets")
-@analyzerModule("njets_category", depends_on=['objects'], categories="axis_cat")
-def nJetsCategory(events, analyzer):
-  analyzer.histogram_builder.addCategory(njets_axis, ak.num(events.good_jets, axis=1))
-  return events, analyzer
+<<<<<<< HEAD
+@analyzerModule("njets_category", ModuleType.Categories)
+def njetCategory(events,data):
+    a = hist.axis.IntCategory([4, 5, 6], name="number_jets", label="NJets")
+    return (a, ak.num(events.good_jets, axis=1))
+
+@analyzerModule("jetpT300", ModuleType.Categories)
+def jetpTCategory(events, data):
+	good_jets = events.good_jets
+	filled_jets = ak.pad_none(good_jets, 4, axis=1)
+	a = hist.axis.IntCategory([0, 1], name = 'jetpT300', label = 'jetpT300')
+	return (a, ak.fill_none(filled_jets[:, 0].pt >= 300, False))
+
+@analyzerModule("nJets456", ModuleType.Categories)
+def nJetReqCategory(events, data):
+	good_jets = events.good_jets
+	a = hist.axis.IntCategory([0, 1], name = 'nJets456', label = 'nJets456')
+	return (a, (ak.num(good_jets) >= 4) & (ak.num(good_jets) <= 6))
 
 
-@analyzerModule("jetpT300", depends_on=['objects'], categories='axis_cat')
-def jetpTCategory(events, analyzer):
-  good_jets = events.good_jets
-  filled_jets = ak.pad_none(good_jets, 4, axis=1)
-  a = hist.axis.IntCategory([0, 1], name = 'jetpT300', label = 'jetpT300')
-  analyzer.histogram_builder.addCategory(a, ak.fill_none(filled_jets[:, 0].pt >= 300, False))
-  return events, analyzer
+@analyzerModule("leptonVeto", ModuleType.Categories)
+def leptonCategory(events, data):
+	good_muons = events.good_muons
+	good_electrons = events.good_electrons
+	a = hist.axis.IntCategory([0, 1], name = 'leptonVeto', label = 'leptonVeto')
+	return (a, (ak.num(good_electrons) == 0) & (ak.num(good_muons) == 0))
 
+@analyzerModule("dRJets24", ModuleType.Categories)
+def dRJetsCategory(events, data):
+	good_jets = events.good_jets
+	filled_jets = ak.pad_none(good_jets, 4, axis=1)
+	top_two_dr = ak.fill_none(filled_jets[:, 0].delta_r(filled_jets[:, 1]), False)
+	a = hist.axis.IntCategory([0, 1], name = 'dRJets24', label = 'dRJets24')
+	return (a, (top_two_dr < 4) & (top_two_dr > 2))
 
-@analyzerModule("nJets456", depends_on=['objects'], categories='axis_cat')
-def nJetReqCategory(events, analyzer):
-  good_jets = events.good_jets
-  a = hist.axis.IntCategory([0, 1], name = 'nJets456', label = 'nJets456')
-  analyzer.histogram_builder.addCategory(a, (ak.num(good_jets) >= 4) & (ak.num(good_jets) <= 6))
-  return events, analyzer
+@analyzerModule("312Bs", ModuleType.Categories)
+def bTag312(events, data):
+	mediumBs = events.med_bs
+	tightBs = events.tight_bs
+	a = hist.axis.IntCategory([0, 1], name = '312Bs', label = '312Bs')
+	return (a, (ak.num(mediumBs) >= 2) & (ak.num(tightBs) >= 1))
 
-@analyzerModule("leptonVeto", depends_on=['objects'], categories='axis_cat')
-def leptonCategory(events, analyzer):
-  good_muons = events.good_muons
-  good_electrons = events.good_electrons
-  a = hist.axis.IntCategory([0, 1], name = 'leptonVeto', label = 'leptonVeto')
-  analyzer.histogram_builder.addCategory(a, (ak.num(good_electrons) == 0) & (ak.num(good_muons) == 0))
-  return events, analyzer
+@analyzerModule("313Bs", ModuleType.Categories)
+def bTag313(events, data):
+	tightBs = events.tight_bs
+	a = hist.axis.IntCategory([0, 1], name = '313Bs', label = '313Bs')
+	return (a, (ak.num(tightBs) >= 3))
 
-@analyzerModule("dRJets24", depends_on=['objects', 'event_level'], categories='axis_cat')
-def dRJetsCategory(events, analyzer):
-  good_jets = events.good_jets
-  filled_jets = ak.pad_none(good_jets, 4, axis=1)
-  top_two_dr = ak.fill_none(filled_jets[:, 0].delta_r(filled_jets[:, 1]), False)
-  a = hist.axis.IntCategory([0, 1], name = 'dRJets24', label = 'dRJets24')
-  analyzer.histogram_builder.addCategory(a, (top_two_dr < 4) & (top_two_dr > 2))
-  return events, analyzer
+@analyzerModule("dRbb_312", ModuleType.Categories)
+def dRbb312(events, data):
+	mediumBs = events.med_bs
+	mediumBs = ak.pad_none(mediumBs, 2, axis = 1)
+	dRbb12 = ak.fill_none(mediumBs[:, 0].delta_r(mediumBs[:, 1]), 0)
+	a = hist.axis.IntCategory([0, 1], name = 'dRbb_312', label = 'dRbb_312')
+	return (a, (dRbb12 >= 1))
 
+@analyzerModule("dRbb_313", ModuleType.Categories)
+def dRbb313(events, data):
+	tightBs = events.tight_bs
+	tightBs = ak.pad_none(tightBs, 2, axis = 1)
+	dRbb12 = ak.fill_none(tightBs[:, 0].delta_r(tightBs[:, 1]), 0)
+	a = hist.axis.IntCategory([0, 1], name = 'dRbb_313', label = 'dRbb_313')
+	return (a, (dRbb12 >= 1))
 
-@analyzerModule("312Bs", depends_on=['objects', 'event_level'], categories='axis_cat')
-def bTag312(events, analyzer):
-  mediumBs = events.med_bs
-  tightBs = events.tight_bs
-  a = hist.axis.IntCategory([0, 1], name = '312Bs', label = '312Bs')
-  analyzer.histogram_builder.addCategory(a, (ak.num(mediumBs) >= 2) & (ak.num(tightBs) >= 1))
-  return events, analyzer
+@analyzerModule("triggers", ModuleType.Selection)
+def triggers(events, selection):
+	selection.add("hlt", (events.HLT.PFHT1050 | events.HLT.AK8PFJet400_TrimMass30).to_numpy())
+	return selection
 
-@analyzerModule("313Bs", depends_on=['objects', 'event_level'], categories='axis_cat')
-def bTag313(events, analyzer):
-  tightBs = events.tight_bs
-  a = hist.axis.IntCategory([0, 1], name = '313Bs', label = '313Bs')
-  analyzer.histogram_builder.addCategory(a, (ak.num(tightBs) >= 3))
-  return events, analyzer
+@analyzerModule("Mu50Trigger", ModuleType.Selection)
+def Mu50Trigger(events, selection):
+	good_electrons = events.good_electrons
+	good_muons = events.good_muons
+	selection.add("muonTrigger", (events.HLT.Mu50).to_numpy())
+	selection.add("electronVeto", ((ak.num(good_electrons) == 0) & (ak.num(good_muons) > 0)).to_numpy())
+	return selection
 
-@analyzerModule("dRbb_312", depends_on=['objects', 'event_level'], categories='axis_cat')
-def dRbb312(events, analyzer):
-  mediumBs = events.med_bs
-  mediumBs = ak.pad_none(mediumBs, 2, axis = 1)
-  dRbb12 = ak.fill_none(mediumBs[:, 0].delta_r(mediumBs[:, 1]), 0)
-  a = hist.axis.IntCategory([0, 1], name = 'dRbb_312', label = 'dRbb_312')
-  analyzer.histogram_builder.addCategory(a, (dRbb12 >= 1))
-  return events, analyzer
-
-@analyzerModule("dRbb_313", depends_on=['objects', 'event_level'], categories='axis_cat')
-def dRbb313(events, analyzer):
-  tightBs = events.tight_bs
-  tightBs = ak.pad_none(tightBs, 2, axis = 1)
-  dRbb12 = ak.fill_none(tightBs[:, 0].delta_r(tightBs[:, 1]), 0)
-  a = hist.axis.IntCategory([0, 1], name = 'dRbb_313', label = 'dRbb_313')
-  analyzer.histogram_builder.addCategory(a, (dRbb12 >= 1))
-  return events, analyzer
-
-@analyzerModule("triggers", categories='selection', depends_on=['objects'])
-def triggers(events, analyzer):
-  selection = analyzer.selection
-  selection.add("hlt", (events.HLT.PFHT1050 | events.HLT.AK8PFJet400_TrimMass30).to_numpy())
-  events = analyzer.applySelection(events)
-  return events, analyzer
-
-@analyzerModule("Mu50Trigger", categories='selection', depends_on=['objects'])
-def Mu50Trigger(events, analyzer):
-  selection = analyzer.selection
-  good_electrons = events.good_electrons
-  good_muons = events.good_muons
-  selection.add("muonTrigger", (events.HLT.Mu50).to_numpy())
-  selection.add("electronVeto", ((ak.num(good_electrons) == 0) & (ak.num(good_muons) > 0)).to_numpy())
-  events = analyzer.applySelection(events)
-  return events, analyzer
-
-@analyzerModule("Mu50SoftDropPlotTrigger", categories='selection', depends_on=['objects'])
-def Mu50SoftDropPlotTrigger(events, analyzer):
-  selection = analyzer.selection
+@analyzerModule("Mu50SoftDropPlotTrigger", ModuleType.Selection)
+def Mu50SoftDropPlotTrigger(events, selection):
   good_electrons = events.good_electrons
   good_muons = events.good_muons
   fatjets = events.FatJet
@@ -112,71 +97,80 @@ def Mu50SoftDropPlotTrigger(events, analyzer):
   selection.add("muonTrigger", (events.HLT.Mu50).to_numpy())
   selection.add("electronVeto", ((ak.num(good_electrons) == 0) & (ak.num(good_muons) > 0)).to_numpy())
   selection.add("highPTJet", (ak.num(fatjets) > 0).to_numpy())
-  events = analyzer.applySelection(events)
-  return events, analyzer
+  return selection
 
-@analyzerModule("pTTrigger", depends_on=['objects'], categories='axis_cat')
-def pTTrigger(events, analyzer):
-  pT400 = events.HLT.AK8PFJet400_TrimMass30
-  a = hist.axis.IntCategory([0, 1], name = 'pT400', label = 'pT400')
-  analyzer.histogram_builder.addCategory(a, pT400)
-  return events, analyzer
+@analyzerModule("pTTrigger", ModuleType.Categories)
+def pTTrigger(events, data):
+	pT400 = events.HLT.AK8PFJet400_TrimMass30
+	a = hist.axis.IntCategory([0, 1], name = 'pT400', label = 'pT400')
+	return (a, pT400)
 
-@analyzerModule('fatJetpTPlot', depends_on=['objects', 'event_level'], categories='main')
-def pTPlot(events, analyzer):
-  fatjets = events.FatJet
-  fatjets = fatjets[(events.FatJet.pt > 175) & (abs(events.FatJet.eta) < 2.4) & (events.FatJet.msoftdrop > 50)]
-  mask = ak.num(fatjets, axis=1) > 0
-  fatjets_mask = fatjets[mask]
-  analyzer.H('pT1', pt_axis, fatjets_mask[:, 0].pt, mask = mask, name = f'p_T of leading AK8 jet', description = f'p_T of leading AK8 jet')
-  return events, analyzer
+@analyzerModule("fatJetpTPlot", ModuleType.MainHist)
+def pTPlot(events, hmaker):
+	ret = {}
+	fatjets = events.FatJet
+	fatjets = fatjets[(events.FatJet.pt > 175) & (abs(events.FatJet.eta) < 2.4) & (events.FatJet.msoftdrop > 50)]
+	mask = ak.num(fatjets, axis=1) > 0
+	fatjets_mask = fatjets[mask]
+	ret[rf"pT1"] = hmaker(
+		pt_axis,
+		fatjets_mask[:, 0].pt,
+		mask = mask,
+		name = f'p_T of leading AK8 jet',
+		description = f'p_T of leading AK8 jet',
+	)
+	return ret
 
-@analyzerModule("softDropMassPlot", depends_on=['objects', 'event_level'], categories='main')
-def mSoftDropMassPlot(events, analyzer):
+@analyzerModule("softDropMassPlot", ModuleType.MainHist)
+def mSoftDropMassPlot(events, hmaker):
+	ret = {}
 	fatjets = events.FatJet
 	fatjets = fatjets[(events.FatJet.pt > 175) & (abs(events.FatJet.eta) < 2.4)]
 	mask = ak.num(fatjets, axis=1) > 0
 	fatjets_mask = fatjets[mask]
-	analyzer.H("mSoftDrop",
+	ret[rf"mSoftDrop"] = hmaker(
 		softdrop_axis,
 		fatjets_mask[:, 0].msoftdrop,
 		mask = mask,
 		name = f'p_T of leading AK8 jet',
 		description = f'p_T of leading AK8 jet',
 	)
-	return events, analyzer
+	return ret
 
-@analyzerModule("softDroppT2DPlot", depends_on=['objects', 'event_level'], categories='main')
-def softDrop2DPlot(events, analyzer):
+@analyzerModule("softDroppT2DPlot", ModuleType.MainHist)
+def softDrop2DPlot(events, hmaker):
+	ret = {}
 	fatjets = events.FatJet
 	fatjets = fatjets[(events.FatJet.pt > 175) & (abs(events.FatJet.eta) < 2.4)]
 	mask = ak.num(fatjets, axis=1) > 0
 	fatjets_mask = fatjets[mask]
-	analyzer.H(rf"mSoftDrop2D",
+	ret[rf"mSoftDrop2D"] = hmaker(
 		[pt_axis, softdrop_axis],
 		[fatjets_mask[:, 0].pt, fatjets_mask[:, 0].msoftdrop],
 		mask = mask,
 		name = f'p_T of leading AK8 jet',
 		description = f'p_T of leading AK8 jet',
 	)
-	return events, analyzer
+	return ret
 
-@analyzerModule("HTTrigger", depends_on=['objects'], categories='cat_axis')
-def HTTrigger(events, analyzer):
+@analyzerModule("HTTrigger", ModuleType.Categories)
+def HTTrigger(events, data):
   HT1050 = events.HLT.PFHT1050
   a = hist.axis.IntCategory([0, 1], name = 'HT1050', label = 'HT1050')
-  analyzer.histogram_builder.addCategory(a, HT1050)
-  return events, analyzer
+  return (a, HT1050)
 
-@analyzerModule("HTTriggerPlot", depends_on=['objects', 'event_level'], categories='main')
-def HTTriggerPlot(events, analyzer):
-	analyzer.H('HT',
+@analyzerModule("HTTriggerPlot", ModuleType.MainHist)
+def HTTriggerPlot(events, hmaker):
+	ret = {}
+	print(events.fields)
+	print(events.event.fields)
+	ret[rf'HT'] = hmaker(
 		ht_axis,
 		events.HT,
 		name = 'HT',
 		description = 'HT',
 	)
-	return events, analyzer
+	return ret
 
 @analyzerModule("pTOrHTTrigger", depends_on=['objects'], categories='cat_axis')
 def pTOrHTTrigger(events, analyzer):
@@ -185,7 +179,6 @@ def pTOrHTTrigger(events, analyzer):
   analyzer.histogram_builder.addCategory(a, triggers)
   return events, analyzer
 
-'''
 @analyzerModule("nMinusOnePlots", ModuleType.MainHist)
 def nMinusOnePlots(events, hmaker):
 	gj = events.good_jets
@@ -240,4 +233,3 @@ def nMinusOnePlots(events, hmaker):
 	)
 
 	return ret
-'''
