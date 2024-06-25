926a7c4e831340807792ac84d5315831f793a1d8
Catherine Welch <catherinewelch@proton.me>
2024-06-25 17:36:57 -0500

    vaguely functional 2d plots

--- peakquality.py
+++ peakquality.py
@@ -24,17 +24,16 @@ def s_over_root_b(hists, hists_background, xvar, true_mass, window_width=-1):
         return _L2_norm(s_over_root_b_points), bin_centers, s_over_root_b_points
     return _L2_norm(s_over_root_b_points[abs(np.add(bin_centers, -true_mass)) < window_width/2]), bin_centers, s_over_root_b_points
 
-def s_over_root_b_2D(hists, hists_background, xvar, true_mass, window_width=-1):
+def s_over_root_b_2D(hists, hists_background, xvar):
     signal_hist = hists[xvar]
     background_hist = hists_background[xvar]
-    print(signal_hist.to_numpy()[0])
-    print(background_hist.to_numpy())
-    signal_data_x, signal_data_y = signal_hist.to_numpy()[0]
-    background_data_x, background_data_y, edges_x, edges_y = background_hist.to_numpy()
-    bin_centers = (edges[:-1] + edges[1:])/2
+    signal_data = signal_hist.to_numpy()[0]
+    background_data, edges_x, edges_y = background_hist.to_numpy()
+
+    bin_centers_x = (edges_x[:-1] + edges_x[1:])/2
+    bin_centers_y = (edges_y[:-1] + edges_y[1:])/2
     sqrt_b = np.sqrt(background_data)
     s_over_root_b_points = np.nan_to_num(signal_data/sqrt_b)
 
-    if window_width == -1: # no window
-        return _L2_norm(s_over_root_b_points), bin_centers, s_over_root_b_points
-    return _L2_norm(s_over_root_b_points[abs(np.add(bin_centers, -true_mass)) < window_width/2]), bin_centers, s_over_root_b_points
+    return _L2_norm(s_over_root_b_points), edges_x, edges_y, s_over_root_b_points
+    # return _L2_norm(s_over_root_b_points[abs(np.add(bin_centers_x, -true_mass)) < window_width/2]), bin_centers, s_over_root_b_points
