f21cafa4bd224092e334876609e27b55c0022ac1
Charles Kapsiak <ckapsiak@cmslpc341.fnal.gov>
2024-07-16 22:18:59 -0500

    Correct error in 2d titles

--- analyzer/plotting/plots_2d.py
+++ analyzer/plotting/plots_2d.py
@@ -47,11 +47,6 @@ def addTitles2D(ax, plot_object):
     ax.set_xlabel(axes[0].title + (f" [{x_unit}]" if x_unit else ""))
     ax.set_ylabel(axes[1].title + (f" [{y_unit}]" if y_unit else ""))
 
-    if np.sum(np.ravel(plot_object.values())) < 20:
-        zlab = "Normalized Events"
-    else:
-        zlab = "Weighted Events"
-
     zlab = FillType.getAxisTitle(plot_object.fill_type)
 
     if x_unit and y_unit and x_unit == y_unit:
