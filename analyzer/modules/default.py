77719d3dbed94f89f256a72942daf92b36718a5d
SethBendigo <sethbendigo@outlook.com>
2024-07-31 15:30:40 -0500

    Cut dictionary to list

--- analyzer/modules/default.py
+++ analyzer/modules/default.py
@@ -1,8 +1,18 @@
 import awkward as ak
 from analyzer.core import analyzerModule
 
-
 @analyzerModule("apply_selection", categories="post_selection", always=True)
 def applySelection(events, analyzer):
     events = analyzer.applySelection(events)
+
+    hlt_names = analyzer.profile.hlt
+    hlt_names = ' | '.join(hlt_names)
+    cut_dict["hlt"] = hlt_names
+    cut_dict = {"ht1200": "HT ≥ 1200", "highptjet": "Jet-PT ≥ 300",
+        "jets": "4 ≤ N-Jets ≤ 6", "0Lep": "0e, 0μ", "0looseb": "0b",
+        "2bjet": "Med-b Jets ≥ 2", "1tightbjet": "Tight-b Jets ≥ 1",
+        "b_dr": "b-jet ΔR > 1", "bbpt": "b-jet 1+2 > 200", "hlt": hlt_names}
+
+    used_cuts = [cut_dict[i] for i in analyzer.selection.names]
+
     return events, analyzer
