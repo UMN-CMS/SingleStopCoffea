3f765824c9916619cc7089dadd4198ea688bb875
Charles Kapsiak <ckapsiak@cmslpc307.fnal.gov>
2024-08-20 09:56:46 -0500

    Fix setup

--- setup.sh
+++ setup.sh
@@ -19,15 +19,17 @@ env_configs[torch,extras]="torch"
 if nvidia-modprobe 2> /dev/null; then 
     env_configs[torch,apptainer_flags]="--nv"
 fi
-#env_configs[torch,container]="/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmsml/cmsml:3.10"
-env_configs[torch,container]="/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmsml/cmsml:3.11-cuda"
+env_configs[torch,container]="/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmsml/cmsml:3.10"
 
 env_configs[jaxenv,venv]="jaxenv"
+env_configs[jaxenv,extras]="torch"
 env_configs[jaxenv,empty]="true"
 if nvidia-modprobe 2> /dev/null; then 
     env_configs[jaxenv,apptainer_flags]="--nv"
 fi
-env_configs[jaxenv,container]="/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmsml/cmsml:3.11-cuda"
+#env_configs[jaxenv,container]="/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmsml/cmsml:3.10"
+#env_configs[jaxenv,container]="/cvmfs/unpacked.cern.ch/registry.hub.docker.com/fnallpc/fnallpc-docker:pytorch-2.0.0-cuda11.7-cudnn8-runtime-singularity"
+env_configs[jaxenv,container]="/cvmfs/unpacked.cern.ch/registry.hub.docker.com/fnallpc/fnallpc-docker:tensorflow-2.12.0-gpu-singularity"
 
 
 function box_out()
@@ -132,16 +134,6 @@ function rcmode(){
     BGW="\[\033[47m\]"
     NONE="\[\033[0m\]"    # unsets color to term's fg color
 
-    local config_name=$1
-    local env=${env_configs[$config_name,venv]}
-
-    if [[ ! $env == "false" ]]; then
-        if [[ ! -d $env ]]; then
-            printf "Virtual environment does not exist, creating virtual environment\n"
-            create_venv "$1"
-        fi
-        activate_venv "$1"
-    fi
 
     [ -z "$PS1" ] && return
 
