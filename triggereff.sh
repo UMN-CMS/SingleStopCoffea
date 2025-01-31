6148bc2c5fe14adbb46ffc13366359acf02e0ead
Shardul Rao <shardulr2931@gmail.com>
2025-01-31 14:24:50 -0600

    Updated trigger eff code

---
+++ triggereff.sh
@@ -0,0 +1,13 @@
+python3 -m analyzer run-samples -s DataSingleMuon2018Official -o "DataSingleMuon2018Official.pkl" -m objects baseline_muon_selection apply_selection trigger_efficiency_hists jets -a localhost:10006
+
+#python3 -m analyzer run-samples -s DataSingleMuon2017 -o "DataSingleMuon2017.pkl" -m objects baseline_muon_selection apply_selection trigger_efficiency_hists jets -a localhost:10006
+
+#python3 -m analyzer run-samples -s DataSingleMuon2016_preVFP -o "DataSingleMuon2016_preVFP.pkl" -m objects baseline_muon_selection apply_selection trigger_efficiency_hists jets -a localhost:10006
+
+#python3 -m analyzer run-samples -s DataSingleMuon2016_postVFP -o "DataSingleMuon2016_postVFP.pkl" -m objects baseline_muon_selection apply_selection trigger_efficiency_hists jets -a localhost:10006
+
+#python3 -m analyzer run-samples -s DataMuon2023 -o "DataMuon2023.pkl" -m objects baseline_muon_selection apply_selection trigger_efficiency_hists jets -a localhost:10006
+
+python3 -m analyzer run-samples -s DataMuon2022PreEE -o "DataMuon2022PreEE.pkl" -m objects baseline_muon_selection apply_selection trigger_efficiency_hists jets -a localhost:10006
+
+python3 -m analyzer run-samples -s DataMuon2022PostEE -o "DataMuon2022PostEE.pkl" -m objects baseline_muon_selection apply_selection trigger_efficiency_hists jets -a localhost:10006
