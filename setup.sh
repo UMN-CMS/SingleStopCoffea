53433616829fcc91f321d321817c154c17e0ff13
Charlie Kapsiak <kapsi002@umn.edu>
2025-02-06 12:21:15 -0600

    Remove binding for UMN

--- setup.sh
+++ setup.sh
@@ -201,7 +201,6 @@ EOF
         if [[ $(hostname) =~ "umn" ]]; then
             apptainer_flags="$apptainer_flags --bind /local/cms/user/"
             apptainer_flags="$apptainer_flags --bind /cvmfs"
-            apptainer_flags="$apptainer_flags --bind /cvmfs/grid.cern.ch/etc/grid-security:/etc/grid-security"
         fi
         if [[ ! -z "${X509_USER_PROXY}" ]]; then
             apptainer_flags="$apptainer_flags --bind ${X509_USER_PROXY%/*}"
