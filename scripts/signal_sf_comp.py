561accf3b4064e058fad481329dc6d7d0c60a105
SethBendigo <sethbendigo@outlook.com>
2025-06-06 11:18:02 -0500

    WIP: SF rebinning

--- scripts/signal_sf_comp.py
+++ scripts/signal_sf_comp.py
@@ -30,9 +30,9 @@ def plot_summed_hist(histogram_dir, coupling):
     unc_values = np.sqrt(summed_hist.variances())
     plt.style.use(hep.style.CMS)
 
+    msd_centers = summed_hist.axes[2].centers
+    pt_centers = summed_hist.axes[1].centers
     for i in range(length):
-        msd_centers = summed_hist.axes[2].centers
-        pt_centers = summed_hist.axes[1].centers
         fig, ax = plt.subplots()
         loop_bin = summed_hist.axes[0][i]
         htbinstr = "_".join([str(k) for k in loop_bin])
@@ -50,9 +50,29 @@ def plot_summed_hist(histogram_dir, coupling):
                     ha="center", va="center",
                     color=text_color, fontsize=12) 
         fig.tight_layout()
-        output_title = f"/srv/postprocessing/trigger_eff_3d_hist_test/plots/2018/Signal{coupling}/signal_{coupling}_{htbinstr}.pdf"
+        output_title = f"/srv/postprocessing/trigger_eff_3d_hist_test/plots/2018/signal_{coupling}_{htbinstr}.pdf"
         fig.savefig(output_title)
         plt.close()
+    
+    fig, ax = plt.subplots()
+    label = f'Signal {coupling} Counts'
+    hep.cms.label(year=year_num,ax=ax,data=True,label=label,com=energy,loc=3)
+    summed_hist.project(1,2).plot2d(ax=ax)
+
+    for x_bin in range(len(pt_centers)):
+        for msd_bin in range(len(msd_centers)):
+            # Auto text color based on background
+            text_color = 'white'
+            sup = f'{np.sqrt(summed_hist.project(1,2)[x_bin,msd_bin].variance):.2f}'
+            sub = sup
+            text = f"${summed_hist.project(1,2)[x_bin,msd_bin].value:.2f}^{{{sup}}}_{{{sub}}}$"
+            ax.text(pt_centers[x_bin], msd_centers[msd_bin], text,
+                ha="center", va="center",
+                color=text_color, fontsize=12) 
+    fig.tight_layout()
+    output_title = f"/srv/postprocessing/trigger_eff_3d_hist_test/plots/2018/signal_{coupling}_summed_ht.pdf"
+    fig.savefig(output_title)
+    plt.close()
 
 for coupling in ['312','313']:
     histogram_dir = Path(f'/srv/postprocessing/trigger_eff_3d_hist_test/plots/2018/Signal{coupling}')
