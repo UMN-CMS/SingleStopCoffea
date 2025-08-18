0a729db897ff78f65bf5feaedf46071820426e3c
Charlie Kapsiak <kapsi002@umn.edu>
2025-08-18 09:40:57 -0500

    Add pt reweighting

--- analyzer/modules/tt_studies.py
+++ analyzer/modules/tt_studies.py
@@ -1,5 +1,6 @@
 import awkward as ak
 import numpy as np
+import dask_awkward as dak
 from analyzer.core import MODULE_REPO, ModuleType
 import hist
 
@@ -27,9 +28,13 @@ def single_lep_selection(events, params, selector):
     passes_jets = (ak.num(good_jets) >= 4) & (ak.num(good_jets) <= 6)
     selector.add("njets", passes_jets)
 
+@MODULE_REPO.register(ModuleType.Selection)
+def high_pt_jet_selection(events, params, selector):
+    """Signal selection without b cuts"""
+    good_jets = events.good_jets
+    filled_jets = ak.pad_none(good_jets, 4, axis=1)
     passes_highptjet = ak.fill_none(filled_jets[:, 0].pt > 300, False)
     selector.add("highptjet", passes_highptjet)
-
 
 @MODULE_REPO.register(ModuleType.Categorization)
 def njets_category(columns, params, categories):
@@ -39,8 +44,24 @@ def njets_category(columns, params, categories):
         values=ak.num(columns.good_jets),
     )
 
+
 def topPtReweightData(pt):
     return np.exp(0.0615 - 0.0005 * pt)
+
+
+def getWeightRatio(new_weights, before_weights):
+    if ak.backend(new_weights) == "typetracer":
+        touched = ak.Array(
+            ak.typetracer.length_zero_if_typetracer(new_weights).layout.to_typetracer(
+                forget_length=True
+            )
+        )
+        return touched
+    else:
+        before_total = ak.sum(before_weights, axis=0)
+        after_total = ak.sum(before_weights * new_weights, axis=0)
+        ratio = before_total / after_total
+        return ratio
 
 
 @MODULE_REPO.register(ModuleType.Weight)
@@ -48,11 +69,12 @@ def top_pt_reweighting(events, params, weight_manager):
 
     gp = events.GenPart
     tops = gp[(abs(gp.pdgId) == 6) & gp.hasFlags("isLastCopy")]
+
     weight = np.sqrt(ak.prod(topPtReweightData(tops.pt), axis=1))
     before_weights = weight_manager.weight()
     before_total = ak.sum(before_weights, axis=0)
     after_total = ak.sum(before_weights * weight, axis=0)
     ratio = before_total / after_total
-
-    # MUST be weight * ratio rather than ratio * weight to avoid typing issues :| 
-    weight_manager.add("top_pt_reweighting",  weight * ratio)
+    weight = weight * ratio
+    # MUST be weight * ratio rather than ratio * weight to avoid typing issues :|
+    weight_manager.add("top_pt_reweighting", weight)
