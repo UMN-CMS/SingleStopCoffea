5b6ce8d58743a6f3af7483e0bbebf8a14da54ffb
Charlie Kapsiak <kapsi002@umn.edu>
2025-09-02 11:32:12 -0500

    New exception type

--- analyzer/core/exceptions.py
+++ analyzer/core/exceptions.py
@@ -6,3 +6,7 @@ class ResultIntegrityError(Exception):
     def __init__(self, message):
         super().__init__(message)
 
+class MultiTaskException(Exception):
+    def __init__(self, message, exceptions):
+        super().__init__(message)
+        self.exceptions = exceptions
