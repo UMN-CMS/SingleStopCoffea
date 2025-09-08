d9db10527fa0ac74d7a157c5596d4f04799a4999
Charlie Kapsiak <kapsi002@umn.edu>
2025-09-08 13:28:39 -0500

    Supress warnings from XRootD

--- analyzer/__init__.py
+++ analyzer/__init__.py
@@ -1,5 +1,7 @@
 import warnings
 from analyzer.configuration import CONFIG
 
+
 if CONFIG.SUPPRESS_WARNINGS:
     warnings.filterwarnings("ignore", module="coffea.*")
+    warnings.filterwarnings("ignore", module="XRootD.*")
