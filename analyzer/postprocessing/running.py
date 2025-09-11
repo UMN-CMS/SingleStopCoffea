64eb08eae6c86ab4c8377f82e485ed6a7732e3d2
Charlie Kapsiak <kapsi002@umn.edu>
2025-09-11 16:56:06 -0500

    Update running for new results scheme

--- analyzer/postprocessing/running.py
+++ analyzer/postprocessing/running.py
@@ -2,9 +2,22 @@ import concurrent.futures as cf
 import itertools as it
 
 import matplotlib as mpl
-from analyzer.core.results import loadSampleResultFromPaths, makeDatasetResults
+from collections import defaultdict
+from analyzer.core.results import (
+    loadSampleResultFromPaths,
+    makeDatasetResults,
+    gatherFilesByPattern,
+)
 from rich import print
-from rich.progress import Progress
+from rich.progress import Progress, track
+from distributed import (
+    Client,
+    LocalCluster,
+    fire_and_forget,
+    get_client,
+    secede,
+    rejoin,
+)
 
 from .plots.mplstyles import loadStyles
 
@@ -48,44 +61,77 @@ def chunks(lst, n):
         yield lst[i : i + n]
 
 
+def processFileGroup(files, postprocessors, use_samples_as_datasets=False, drops=None):
+    all_needed_hists = [y for x in postprocessors for y in x.getNeededHistograms()]
+    sample_results = loadSampleResultFromPaths(
+        files, include=all_needed_hists, show_progress=False
+    )
+
+    def dropSampleFunction(sid):
+        if drops is None:
+            return False
+        return drops.match(sid.sample_name)
+
+    dataset_results = makeDatasetResults(
+        sample_results,
+        drop_sample_fn=dropSampleFunction,
+        include_samples_as_datasets=use_samples_as_datasets,
+    )
+    sector_results = list(
+        it.chain.from_iterable(r.sector_results for r in dataset_results.values())
+    )
+    ret = []
+    client = get_client()
+    for x in it.chain.from_iterable(
+        (processor.getExe(sector_results) for processor in postprocessors)
+    ):
+        fire_and_forget(client.submit(x))
+
+
+def assembleFileGroup(param_dict, fields):
+    ret = defaultdict(set)
+    for k, v in param_dict.items():
+        newk = frozenset((x, y) for x, y in k if x in fields)
+        ret[newk] |= v
+    return ret
+
+
 def runPostprocessors(config, input_files, parallel=8):
     loadStyles()
     print("Loading Postprocessors")
     loaded, catalog, drops, use_samples_as_datasets, num_files = loadPostprocessors(
         config
     )
+    for processor in loaded:
+        processor.init()
     all_needed_hists = [y for x in loaded for y in x.getNeededHistograms()]
     print("Loading Samples")
 
-    if num_files:
-        to_run = chunks(input_files, num_files)
-    else:
-        to_run = [input_files]
+    all_fields = set()
+    by_fields = defaultdict(list)
+    for processor in loaded:
+        f = processor.getFileFields()
+        all_fields |= f
+        by_fields[frozenset(f)].append(processor)
+    gathered = gatherFilesByPattern(input_files, all_fields)
+    with LocalCluster(dashboard_address="localhost:8789") as cluster, Client(
+        cluster
+    ) as client:
 
-    for files in to_run:
-        sample_results = loadSampleResultFromPaths(
-            files, include=all_needed_hists, show_progress=True
-        )
-
-        def dropSampleFunction(sid):
-            if drops is None:
-                return False
-            return drops.match(sid.sample_name)
-
-        dataset_results = makeDatasetResults(
-            sample_results,
-            drop_sample_fn=dropSampleFunction,
-            include_samples_as_datasets=use_samples_as_datasets,
-        )
-        print("Ready to Process ")
-        sector_results = list(
-            it.chain.from_iterable(r.sector_results for r in dataset_results.values())
-        )
-        for processor in loaded:
-            processor.init()
-        run(
-            it.chain.from_iterable(
-                (processor.getExe(sector_results) for processor in loaded)
-            ),
-            parallel,
-        )
+        for keys, processors in by_fields.items():
+            fgroups = assembleFileGroup(gathered, keys)
+            # print(fgroups)
+            # return
+            for files in fgroups.values():
+                print(files)
+                client.submit(
+                    processFileGroup,
+                    files,
+                    processors,
+                    use_samples_as_datasets=use_samples_as_datasets,
+                    drops=drops,
+                )
+        client.close()
+        cluster.close()
+        print("HERE")
+    print("HERE1")
