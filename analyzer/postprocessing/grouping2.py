eacb345c56f09eea8e6e448de28bf48de7b3b870
kapsiak <kapsi002@umn.edu>
2026-01-04 16:42:09 -0600

    More work

--- analyzer/postprocessing/grouping2.py
+++ analyzer/postprocessing/grouping2.py
@@ -6,6 +6,7 @@ from analyzer.utils.structure_tools import (
     ItemWithMeta,
     commonDict,
 )
+from analyzer.utils.structure_tools import globWithMeta
 from attrs import define
 
 ResultSet = list[list[ItemWithMeta]]
@@ -49,46 +50,48 @@ class GroupBuilder:
 
 @define
 class Histogram1D:
-    inputs: GroupBuilder
+    inputs: list[tuple[str,...]]
+    structure: GroupBuilder
 
-    # style_set: str | StyleSet
-    # output_name: str
-    # 
-    # to_stack: PatternExpression | None = None
-    # 
-    # scale: Literal["log", "linear"] = "linear"
-    # normalize: bool = False
-    # plot_configuration: PlotConfiguration | None = None
 
-    def getRunFunc(self, group):
-        common_meta = commonDict(group)
-
-        for name, sector_pipeline in it.product(self.histogram_names, pipelines):
-            histograms = sector_pipeline.getHists(name)
-            if not histograms:
+    def run(self, data):
+        for i in self.inputs:
+            print(i)
+            items = globWithMeta(data, i)
+            for x in self.structure.apply(items):
+                yield from self.getRunFuncs(x) 
                 return
-            provenance = histograms[0].provenance
-            output_path = doFormatting(self.output_name, **provenance.allEntries())
-            stacked_hists = None
-            if self.to_stack is not None:
-                stacked_hists = [
-                    x for x in histograms if self.to_stack.match(x.sector_parameters)
-                ]
-                histograms = [
-                    x
-                    for x in histograms
-                    if not self.to_stack.match(x.sector_parameters)
-                ]
 
-            pc = self.plot_configuration.makeFormatted(**provenance.allEntries())
-            yield ft.partial(
-                plotOne,
-                histograms,
-                provenance,
-                output_path,
-                scale=self.scale,
-                style_set=self.style_set,
-                normalize=self.normalize,
-                plot_configuration=pc,
-                stacked_hists=stacked_hists,
-            )
+    def getRunFuncs(self, group):
+        group = group["numerator"]
+        common_meta = commonDict(group)
+        print(common_meta)
+        # for name, sector_pipeline in it.product(self.histogram_names, pipelines):
+        #     histograms = sector_pipeline.getHists(name)
+        #     if not histograms:
+        #         return
+        #     provenance = histograms[0].provenance
+        #     output_path = doFormatting(self.output_name, **provenance.allEntries())
+        #     stacked_hists = None
+        #     if self.to_stack is not None:
+        #         stacked_hists = [
+        #             x for x in histograms if self.to_stack.match(x.sector_parameters)
+        #         ]
+        #         histograms = [
+        #             x
+        #             for x in histograms
+        #             if not self.to_stack.match(x.sector_parameters)
+        #         ]
+        # 
+        #     pc = self.plot_configuration.makeFormatted(**provenance.allEntries())
+        #     yield ft.partial(
+        #         plotOne,
+        #         histograms,
+        #         provenance,
+        #         output_path,
+        #         scale=self.scale,
+        #         style_set=self.style_set,
+        #         normalize=self.normalize,
+        #         plot_configuration=pc,
+        #         stacked_hists=stacked_hists,
+            # )
