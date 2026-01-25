af24dcd006072922e0c7da119e586cb627806999
kapsiak <kapsi002@umn.edu>
2026-01-25 11:10:19 -0600

    add ext

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
+sys.path.append(os.path.abspath("./_ext"))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,6 +25,7 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.viewcode",
     "sphinx_autodoc_typehints",
+    "dataset_doc",
 ]
 
 autosummary_generate = True
