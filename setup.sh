7079ef3a791e1c40a4cee5c2df7bfd2176102af5
Joe Gehrke <gehrke@umn.edu>
2026-02-13 18:04:28 -0600

    Bind d1 d2 AND d3 for nobackup to work

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 container="/cvmfs/unpacked.cern.ch/registry.hub.docker.com/coffeateam/coffea-dask-almalinux9:2025.10.2-py3.12"
 
-apptainer exec -B $HOME,/uscmst1b_scratch,$(realpath $HOME),/uscms_data/d3/,$(realpath .),/cvmfs,/etc/condor/,/usr/local/bin/cmslpc-local-conf.py "$container" /bin/bash 
+apptainer exec -B $HOME,/uscmst1b_scratch,$(realpath $HOME),/uscms_data/,$(realpath .),/cvmfs,/etc/condor/,/usr/local/bin/cmslpc-local-conf.py "$container" /bin/bash 
